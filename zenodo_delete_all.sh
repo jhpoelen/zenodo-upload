cb64a7ea69be6d21f7211cddd5caa7f1f0743c5a
jhpoelen <jhpoelen@xs4all.nl>
2021-04-05 13:45:43 -0700

    reduce log level; use file handle to reduce exposure to access token; related to https://github.com/jhpoelen/zenodo-upload/issues/4 fyi @adeslatt

--- zenodo_delete_all.sh
+++ zenodo_delete_all.sh
@@ -4,13 +4,13 @@
 # usage: ./zenodo_delete_all.sh [deposition id] 
 #
 
-set -xe
+set -e
 
 # strip deposition url prefix if provided; see https://github.com/jhpoelen/zenodo-upload/issues/2#issuecomment-797657717
 DEPOSITION=$( echo $1 | sed 's+^http[s]*://zenodo.org/deposit/++g' )
 FILEPATH=$2
 FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
 
-curl -H "Accept: application/json" -H "Authorization: Bearer $ZENODO_TOKEN" "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION/files" | jq --raw-output .[].id > file_ids.txt
+curl -H @<(echo -e "Accept: application/json\nAuthorization: Bearer $ZENODO_TOKEN") "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION/files" | jq --raw-output .[].id > file_ids.txt
 
 cat file_ids.txt | parallel "echo curl -XDELETE -H \\\"Authorization: Bearer $ZENODO_TOKEN\\\" \\\"https://www.zenodo.org/api/deposit/depositions/$DEPOSITION/files/{1}\\\"" > delete_cmds.sh
--- zenodo_upload.sh
+++ zenodo_upload.sh
@@ -4,14 +4,14 @@
 # usage: ./zenodo_upload.sh [deposition id] [filename]
 #
 
-set -xe
+set -e
 
 # strip deposition url prefix if provided; see https://github.com/jhpoelen/zenodo-upload/issues/2#issuecomment-797657717
 DEPOSITION=$( echo $1 | sed 's+^http[s]*://zenodo.org/deposit/++g' )
 FILEPATH=$2
 FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
 
-BUCKET=$(curl -H "Accept: application/json" -H "Authorization: Bearer $ZENODO_TOKEN" "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION" | jq --raw-output .links.bucket)
+BUCKET=$(curl -H @<(echo -e "Accept: application/json\nAuthorization: Bearer $ZENODO_TOKEN") "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION" | jq --raw-output .links.bucket)
 
 
 curl --progress-bar -o /dev/null --upload-file $FILEPATH $BUCKET/$FILENAME?access_token=$ZENODO_TOKEN
