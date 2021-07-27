284bd2b314580f3b423c9b641aea3bb531fcfb19
Ramón Casero <rcasero@gmail.com>
2021-07-27 12:53:52 +0100

    fix bug, process correctly filenames with whitespaces

--- zenodo_upload.sh
+++ zenodo_upload.sh
@@ -8,10 +8,10 @@ set -e
 
 # strip deposition url prefix if provided; see https://github.com/jhpoelen/zenodo-upload/issues/2#issuecomment-797657717
 DEPOSITION=$( echo $1 | sed 's+^http[s]*://zenodo.org/deposit/++g' )
-FILEPATH=$2
+FILEPATH="$2"
 FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
 
 BUCKET=$(curl -H @<(echo -e "Accept: application/json\nAuthorization: Bearer $ZENODO_TOKEN") "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION" | jq --raw-output .links.bucket)
 
 
-curl --progress-bar -o /dev/null -H @<(echo -e "Authorization: Bearer $ZENODO_TOKEN") --upload-file $FILEPATH $BUCKET/$FILENAME
+curl --progress-bar -o /dev/null -H @<(echo -e "Authorization: Bearer $ZENODO_TOKEN") --upload-file "$FILEPATH" $BUCKET/"$FILENAME"
