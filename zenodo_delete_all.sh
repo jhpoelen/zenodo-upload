24806acb0eca311fa127f1999cfb91a1c05f4684
Jorrit Poelen <jhpoelen@xs4all.nl>
2021-08-12 10:40:17 -0700

    remove unused variables.

--- zenodo_delete_all.sh
+++ zenodo_delete_all.sh
@@ -8,8 +8,6 @@ set -e
 
 # strip deposition url prefix if provided; see https://github.com/jhpoelen/zenodo-upload/issues/2#issuecomment-797657717
 DEPOSITION=$( echo $1 | sed 's+^http[s]*://zenodo.org/deposit/++g' )
-FILEPATH=$2
-FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
 
 curl -H @<(echo -e "Accept: application/json\nAuthorization: Bearer $ZENODO_TOKEN") "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION/files" | jq --raw-output .[].id > file_ids.txt
 
